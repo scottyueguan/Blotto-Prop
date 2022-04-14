d5da18a41376a10685e979288ffd3cc2d1652222
Scott Guan <yguan44@gatech.edu>
2022-04-13 23:35:32 -0400

    added graph visualziation.

--- graph_generators.py
+++ graph_generators.py
@@ -1,4 +1,21 @@
 import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+class Graph:
+    def __init__(self, connectivity):
+        self.connectivity = connectivity
+        self.N = connectivity.shape[0]
+
+    def visualize_graph(self):
+        G = nx.DiGraph()
+        for i in range(self.N):
+            for j in range(self.N):
+                if self.connectivity[i, j] > 0:
+                    G.add_edge(str(i), str(j))
+        nx.draw(G, arrows=True, with_labels=True, node_color='w', edgecolors='k')
+        plt.show()
 
 
 def ring_graph_generator(size, undirected=True):
@@ -34,7 +51,6 @@ def random_graph_generator(size, self_loop=False, undirected=False):
     n_nodes = size
     connectivity = np.zeros((size, size))
 
-
     done_flag = False
 
     while not done_flag:
@@ -47,7 +63,7 @@ def random_graph_generator(size, self_loop=False, undirected=False):
                 random_vec = np.random.random((size - i))
                 connectivity[i, i:] = np.array([1 if ele < 0.2 else 0 for ele in random_vec])
             for i in range(n_nodes):
-                connectivity[i,:i] = connectivity.T[i, :i]
+                connectivity[i, :i] = connectivity.T[i, :i]
 
         if self_loop:
             for i in range(n_nodes):
@@ -69,15 +85,19 @@ def random_graph_generator(size, self_loop=False, undirected=False):
 
 def generate_graph(type: str, size: int, **kwargs):
     if type == "ring":
-        return ring_graph_generator(size, **kwargs)
+        connectivity = ring_graph_generator(size, **kwargs)
+        graph = Graph(connectivity)
+        return graph
     if type == "random":
-        return random_graph_generator(size, **kwargs)
+        connectivity = random_graph_generator(size, **kwargs)
+        graph = Graph(connectivity)
+        return graph
 
     else:
         raise Exception("type error!")
 
 
 if __name__ == "__main__":
-    connectivity = generate_graph(type="random", size=10, undirected=True, self_loop=False)
-
-    print(connectivity)
+    graph = generate_graph(type="random", size=10, undirected=False, self_loop=False)
+    graph.visualize_graph()
+    print(graph.connectivity)
